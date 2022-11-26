a1bdca118e59c0c531cc158cef2540c57c37f4b2
yeongbin <yeongbin6483@gmail.com>
2022-11-26 22:13:40 +0900

    ch 03 / 07. 알림 기능 테스트 작성하기 커밋

--- src/main/java/com/example/sns/service/UserService.java
+++ src/main/java/com/example/sns/service/UserService.java
@@ -9,6 +9,8 @@ import com.example.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -60,6 +62,11 @@ public class UserService {
         String token = JwtTokenUtils.generateToken(userName, secretKey, expiredTimeMs);
 
         return token;
+    }
+
+    // TODO : alarm return
+    public Page<Void> alarmList(String userName, Pageable pageable){
+        return Page.empty();
     }
 
 }
